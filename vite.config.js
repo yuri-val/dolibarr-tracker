51939142241947e2a80cead76e6622c9d1bd0900
Yuri V <yuri.valigursky@gmail.com>
2022-04-19 20:18:00 +0300

    Action to deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "https://yuri-val.github.io/dolibarr-tracker/",
 
   // [vuestic-ui] Add alias for ~normalize.css.
   resolve: {
