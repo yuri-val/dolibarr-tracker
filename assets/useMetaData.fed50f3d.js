510f3d5847423ac2f7746ef14b171a8bb278ffac
yuri-val <yuri-val@users.noreply.github.com>
2022-04-22 11:49:12 +0000

    Deploy to GitHub pages

---
+++ assets/useMetaData.fed50f3d.js
@@ -0,0 +1,1 @@
+import{u as t}from"./@vueuse.450f3a45.js";const u=(a,{title:e,breadcrumbs:s})=>{t(`DT | ${e}`),a.breadcrumbs=s};export{u};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dolibarr-tracker/sw.js', { scope: '/dolibarr-tracker/' })})}
