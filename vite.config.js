75d4076c99c614a4022d3cfece34762c7649fda9
Yuri V <yuri.valigursky@gmail.com>
2022-04-22 14:47:59 +0300

    v 1.2.1 PWA Config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,49 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+
+
+const pwaConfig = {
+  includeAssets: [
+    "favicon.svg",
+    "favicon.ico",
+    "robots.txt",
+    "apple-touch-icon.png",
+  ],
+  manifest: {
+    name: "Dolibarr Tracker",
+    short_name: "DT",
+    description:
+      "Web interface for quick time tracking by tasks in the Dolibarr ERP system",
+    theme_color: "#2c82e0",
+    icons: [
+      {
+        src: "android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA(pwaConfig),
+  ],
   base: "/dolibarr-tracker",
   build: {
     rollupOptions: {
